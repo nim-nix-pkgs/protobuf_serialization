57220d5c663ef5ece6178fff01f75c1e0dedcf50
hugosenari <hugosenari@gmail.com>
2022-05-12 11:56:24 -0300

    chore: re index protobuf_serialization

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."protobuf_serialization-master".dir   = "master";
+  inputs."protobuf_serialization-master".owner = "nim-nix-pkgs";
+  inputs."protobuf_serialization-master".ref   = "master";
+  inputs."protobuf_serialization-master".repo  = "protobuf_serialization";
+  inputs."protobuf_serialization-master".type  = "github";
+  inputs."protobuf_serialization-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."protobuf_serialization-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
